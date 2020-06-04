82ee4fc015653b8efb4bf7ed55624cc4075ca1eb
ottonove <niigata01web@gmail.com>
2020-06-04 23:37:47 +0900

    success to gdb debug.

---
+++ data/build/CMakeFiles/sample-project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample-project.dir/main.cpp.o"
+  "sample-project.pdb"
+  "sample-project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ data/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+int main(int, char**) {
+    fs::path txtPath("sample.cpp");
+    cout << fs::exists(txtPath) << endl;
+    cout << fs::absolute(txtPath) << endl;
+
+    std::vector<int> v = {1, 2, 3, 4, 5};
+
+    std::cout << "Hello, world!\n";
+    int i = 0;
+    while(i < 10){
+        std::cout << i++;
+    }
+
+    return 0;
+}
